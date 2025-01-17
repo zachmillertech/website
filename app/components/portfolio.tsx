fa1d88623d51609be3e253294ecb241c0c68aea5
zachmillertech <account@zachmiller.tech>
2025-01-17 10:28:26 -0500

    addl photos

--- app/components/portfolio.tsx
+++ app/components/portfolio.tsx
@@ -5,17 +5,17 @@ const projects = [
   {
     title: "E-commerce Platform Optimization",
     description: "Improved load times by 40% and increased conversion rates by 25%",
-    image: "https://source.unsplash.com/300x200/?puppy,1",
+    image: "https://geniusvets.s3.amazonaws.com/gv-dog-breeds/chinese-shar-pei-1.jpg",
   },
   {
     title: "Cloud Migration for Financial Services",
     description: "Seamlessly migrated legacy systems to AWS, reducing operational costs by 30%",
-    image: "https://source.unsplash.com/300x200/?puppy,2",
+    image: "https://geniusvets.s3.amazonaws.com/gv-dog-breeds/chinese-shar-pei-1.jpg",
   },
   {
     title: "Cybersecurity Overhaul",
     description: "Implemented robust security measures, preventing potential data breaches",
-    image: "https://source.unsplash.com/300x200/?puppy,3",
+    image: "https://geniusvets.s3.amazonaws.com/gv-dog-breeds/chinese-shar-pei-1.jpg",
   },
 ]
 
