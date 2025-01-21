7612bcf25cc9af0652a717edd0526ef298fb2b76
zachmillertech <account@zachmiller.tech>
2025-01-20 21:59:40 -0500

    some updates

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -8,14 +8,14 @@ export default function Hero() {
         className="absolute inset-0 z-0 bg-cover bg-center"
         style={{
           backgroundImage: "url('https://m.media-amazon.com/images/I/71L+46v5hnL.jpg')",
-          opacity: 0.3
+          opacity: 0.75
         }}
         aria-hidden="true"
       ></div>
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between relative z-10">
         <h2 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-          <span className="block">Transform Your</span>
-          <span className="block text-blue-600">Digital Footprint</span>
+          <span className="block">Tech got you in wrinkes?</span> <!-- tech wrinkes slowing you down? Tech wrinkes driving you barking mad? -->
+          <span className="block text-blue-600">The <style class="strikethrough">bark</style> buck stops with my highly solution-focused Tech Consulting Services.</span>
         </h2>
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
