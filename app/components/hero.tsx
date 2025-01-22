7e6673dc3302a4dfa276b151798584a793cdc120
zachmillertech <account@zachmiller.tech>
2025-01-22 10:43:10 -0500

    cute updates

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -12,13 +12,13 @@ export default function Hero() {
       />
       <div className="relative z-10 max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-          <span className="block xl:inline bg-primary text-primary-foreground px-2">Welcome to our</span>{" "}
+          <span className="block xl:inline bg-primary text-primary-foreground px-2">Tech got you in wrinkles?</span>{" "}
           <span className="block text-indigo-600 xl:inline">
-            <span className="line-through">Amazing</span> App
+            The <span className="line-through">bark</span> buck stops here.
           </span>
         </h1>
         <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-          Discover the power of our application and boost your productivity.
+          Is your web presence a little ruff around the edges? I can help transform your digital paw-print.
         </p>
         <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
           <div className="rounded-md shadow">
