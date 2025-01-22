a9c6345ba1e015b38f5e56b6b89ff3d9a6aa49ac
zachmillertech <account@zachmiller.tech>
2025-01-22 11:58:07 -0500

    img fix

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -7,14 +7,14 @@ export default function Hero() {
         className="absolute inset-0 z-0 bg-cover bg-center"
         style={{
           backgroundImage: "url('https://geniusvets.s3.amazonaws.com/gv-dog-breeds/chinese-shar-pei-1.jpg')",
-          opacity: 0.5,
+          opacity: 1,
         }}
       />
       <div className="relative z-10 max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
           <span className="xl bg-indigo-60 text-primary-foreground px-2">Tech got you in wrinkles?</span>{" "}
           <span className="block text-primary xl:inline">
-            The <span className="line-through">baark</span> buck stops here.
+            The <span className="line-through">bark</span> buck stops here.
           </span>
         </h1>
         <p className="mt-3 max-w-md mx-auto text-base text-foreground sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
